d1a689f2a52ac55b7c635ce035a6efd45b1b886b
MaximeMerrien <maxime.merrien@imie.fr>
2019-09-10 14:49:55 +0200

    Added first riddle + timer

---
+++ Assets/Scripts/RiddleResult.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RiddleResult : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button rightChoiceRiddle1 = GameObject.Find("Choix2").GetComponent<Button>();
+        rightChoiceRiddle1.onClick.AddListener(Success);
+    }
+
+    void Success()
+    {
+        Debug.Log("Bonne réponse !");
+    }
+}
---
+++ Assets/Scripts/TimerCount.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCount : MonoBehaviour
+{
+    #region Attributes
+    private Text timer;
+    private bool keepTicking = true;
+    [SerializeField]
+    private float maxTime;
+    #endregion
+
+    #region Methods
+    public float MaxTime
+    {
+        get { return maxTime; }
+        set { maxTime = value; }
+    }
+    #endregion
+
+    #region Functions
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(maxTime == 0f)
+        {
+            maxTime = 600f;
+        }
+
+        timer = GameObject.Find("Timer").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keepTicking)
+        {
+            maxTime -= Time.deltaTime;
+            timer.text = "Temps restant : " + Mathf.RoundToInt(maxTime);
+        }
+        if (maxTime < 0)
+        {
+            keepTicking = false;
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("Choix"))
+            {
+                go.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+    #endregion
+}
